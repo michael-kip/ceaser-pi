518fa6da90aeb3d0e9ae17e503efdbd9e997487a
Michael Kiprotich <michael.kiprotich@student.moringaschool.com>
2022-01-22 21:29:55 +0300

    adding scanner and buffered readser classes to be used to gather input from the user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-
+        BufferedReader myBufferedReader = new BufferedReader( new InputStreamReader(System.in));
+        Scanner myScanner = new Scanner(System.in);
+        boolean isRunCipherApp = true;
     }
 }
+
---
+++ src/main/java/encrypt.java
@@ -0,0 +1,2 @@
+public class encrypt {
+}

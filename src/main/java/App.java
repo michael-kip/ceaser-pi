0f998e44795804c16a9888604926fa9d9e651013
michael-kip <michaelkiprotich@student.moringaschool.com>
2022-01-23 15:11:46 +0300

    adding logic for decryption

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class App {
-    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static void main(String[] args) {
         BufferedReader myBufferedReader = new BufferedReader( new InputStreamReader(System.in));
         Scanner myScanner = new Scanner(System.in);
--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,4 +1,5 @@
 public class Encrypt {
+    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private String inputForEncryption;
     private int encryptionKey;
 
@@ -12,6 +13,18 @@ public class Encrypt {
     public Encrypt(String inputForEncryption, int encryptionKey){
         this.inputForEncryption = inputForEncryption;
         this.encryptionKey = encryptionKey;
+    }
+    public String Encrypt(String inputForEncryption, int encryptionKey){
+        inputForEncryption = inputForEncryption.toUpperCase();
+        String encryptionResults = "";
+        for (int i=0; i<inputForEncryption.length(); i++){
+            int characterIndex = alphabet.indexOf(inputForEncryption.charAt(i));
+            int newCharacterIndex = (characterIndex + encryptionKey)% 26;
+            char encryptedChar = alphabet.charAt(newCharacterIndex);
+            encryptionResults = encryptionResults + encryptedChar;
+        }
+        return encryptionResults;
 
     }
+
 }

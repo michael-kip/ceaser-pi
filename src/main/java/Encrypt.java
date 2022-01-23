de108e538c1ddd0794de50cd01afa4a2efb37895
michael-kip <michaelkiprotich@student.moringaschool.com>
2022-01-23 13:59:51 +0300

    adding getters and encrypt constructor

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,3 +1,17 @@
 public class Encrypt {
-    private String
+    private String inputForEncryption;
+    private int encryptionKey;
+
+    public String getInputForEncryption(){
+        return inputForEncryption;
+    }
+    public int getEncryptionKey(){
+        return encryptionKey;
+    }
+
+    public Encrypt(String inputForEncryption, int encryptionKey){
+        this.inputForEncryption = inputForEncryption;
+        this.encryptionKey = encryptionKey;
+
+    }
 }

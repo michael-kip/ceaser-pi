6a8907b5c17c7f52f4c76c21780b7d64ae29b280
michael-kip <michaelkiprotich@student.moringaschool.com>
2022-01-23 22:41:15 +0300

    Adding junit test for encrypt class to to check if it instantiates correctly

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EncryptTest {
+    @Test
+    public void newEncrypt_instantiatesCorrectly(){
+        Encrypt testEncrypt = new Encrypt("baba", 1 );
+        assertEquals(true, testEncrypt instanceof Encrypt);
+    }
 
 }
